3bd8d90e39c87e01767aeb06d4f0d76ffb6b2958
Pranay Singh <143025694+Fizzzzy18@users.noreply.github.com>
2023-09-29 07:30:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="my-4 flex justify-center gap-2 text-xl font-medium items-center h-[60px] bg-white  rounded-lg">
-            <img src="/logos_contact.png" />
+            <img src="./logos_contact.png" />
           <h1>Contact Hub</h1>
         </div>
   )
